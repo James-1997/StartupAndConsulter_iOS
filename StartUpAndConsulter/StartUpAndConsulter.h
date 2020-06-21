323551b3a0f74d051e7a6ab923e0b48d247bdf71
Robson James <james.robson.junior@gmail.com>
2020-06-21 09:46:28 -0400

    Initial Commit

---
+++ StartUpAndConsulter/StartUpAndConsulter.h
@@ -0,0 +1,19 @@
+//
+//  StartUpAndConsulter.h
+//  StartUpAndConsulter
+//
+//  Created by Robson James Junior on 21/06/20.
+//  Copyright © 2020 robsonJames. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StartUpAndConsulter.
+FOUNDATION_EXPORT double StartUpAndConsulterVersionNumber;
+
+//! Project version string for StartUpAndConsulter.
+FOUNDATION_EXPORT const unsigned char StartUpAndConsulterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StartUpAndConsulter/PublicHeader.h>
+
+
